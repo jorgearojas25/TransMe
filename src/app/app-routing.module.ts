704d4996592a5a8767123a270d6e7a14e88f33d7
Jorge <jorgearojas25@gmail.com>
2019-04-08 19:11:24 -0500

    Construccion de la pagina de inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
 
 const routes: Routes = [
-    { path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] },
-    { path: 'login', loadChildren: './login/login.module#LoginModule' },
+    {path:'', loadChildren:'./home/home.module#HomeModule'},
+    { path: 'home', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard]},
+    { path: 'login', loadChildren: './login/login.module#LoginModule'},
     { path: 'signup', loadChildren: './signup/signup.module#SignupModule' },
     { path: 'error', loadChildren: './server-error/server-error.module#ServerErrorModule' },
     { path: 'access-denied', loadChildren: './access-denied/access-denied.module#AccessDeniedModule' },
