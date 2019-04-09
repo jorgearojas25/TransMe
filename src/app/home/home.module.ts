fe34b6c55781ada73409ca6d8d77701ac253f6aa
Jorge <jorgearojas25@gmail.com>
2019-04-08 22:13:17 -0500

    Barra de navegación agregada al inicio

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { HomeheaderComponent } from './home/homeheader/homeheader.component';
+
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeheaderComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    TranslateModule,
+    NgbDropdownModule
   ]
 })
 export class HomeModule { }
---
+++ src/app/home/home/homeheader/homeheader.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+@Component({
+  selector: 'app-homeheader',
+  templateUrl: './homeheader.component.html',
+  styleUrls: ['./homeheader.component.scss']
+})
+export class HomeheaderComponent implements OnInit {
+
+  public pushRightClass: string;
+
+  constructor(private translate: TranslateService, public router: Router) {
+
+      this.router.events.subscribe(val => {
+          if (
+              val instanceof NavigationEnd &&
+              window.innerWidth <= 992 &&
+              this.isToggled()
+          ) {
+              this.toggleSidebar();
+          }
+      });
+  }
+
+  ngOnInit() {
+      this.pushRightClass = 'push-right';
+  }
+
+  isToggled(): boolean {
+      const dom: Element = document.querySelector('body');
+      return dom.classList.contains(this.pushRightClass);
+  }
+
+  toggleSidebar() {
+      const dom: any = document.querySelector('body');
+      dom.classList.toggle(this.pushRightClass);
+  }
+
+  rltAndLtr() {
+      const dom: any = document.querySelector('body');
+      dom.classList.toggle('rtl');
+  }
+
+  onLoggedout() {
+      localStorage.removeItem('isLoggedin');
+  }
+
+  changeLang(language: string) {
+      this.translate.use(language);
+  }
+
+}
