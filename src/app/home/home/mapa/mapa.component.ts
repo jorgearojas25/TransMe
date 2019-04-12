9f8f90471390ea5a4f954054ced4b4a993cf3a4f
JULIAN <juliantamayo16@outlook.es>
2019-04-12 00:43:57 -0500

    Inicio de mapa

--- src/app/home/home/mapa/mapa.component.ts
+++ src/app/home/home/mapa/mapa.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var ol: any;
+
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html',
   styleUrls: ['./mapa.component.scss']
 })
 export class MapaComponent implements OnInit {
+  latitude = -74.0723172873;
+  longitude = 4.711977791360;
 
-  constructor() { }
-
+  map: any;
   ngOnInit() {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([this.latitude, this.longitude]),
+        zoom: 11
+      })
+    });
   }
-
+  constructor() {}
 }
+
