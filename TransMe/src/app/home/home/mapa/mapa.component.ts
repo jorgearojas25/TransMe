9eb69993d650141727ec5bbd59ee26b9724890d9
julian tamayo <juliantamayo16@outlook.es>
2019-04-25 16:22:21 -0500

    se creo el zoom a los markets

--- TransMe/src/app/home/home/mapa/mapa.component.ts
+++ TransMe/src/app/home/home/mapa/mapa.component.ts
@@ -9,6 +9,7 @@ import { RutasService } from 'src/app/shared/services/rutas.service';
 
 
 
+
 declare var lat: any;
 declare var $: any;
 const jsonDA: any[] = [];
@@ -22,23 +23,27 @@ const arrayRutas: any[] = [];
   styleUrls: ['./mapa.component.scss']
 })
 export class MapaComponent implements OnInit {
+  lat = 4.6597100;
+  lng = -74.0917500;
+  zoom = 11;
    Troncal: any[] = [];
   Estacion: any[]  = [];
   Troncal2: String[];
    // tslint:disable-next-line:max-line-length
-   style2 = '[{"featureType":"poi","elementType":"all","stylers":[{"hue":"#000000"},{"saturation":-100},{"lightness":-100},{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#000000"},{"saturation":-100},{"lightness":-100},{"visibility":"off"}]},{"featureType":"administrative","elementType":"all","stylers":[{"hue":"#000000"},{"saturation":0},{"lightness":-100},{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"hue":"#000000"},{"saturation":-100},{"lightness":-100},{"visibility":"off"}]},{"featureType":"road.local","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"on"}]},{"featureType":"transit","elementType":"labels","stylers":[{"hue":"#000000"},{"saturation":0},{"lightness":-100},{"visibility":"off"}]},{"featureType":"landscape","elementType":"labels","stylers":[{"hue":"#000000"},{"saturation":-100},{"lightness":-100},{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbbbbb"},{"saturation":-100},{"lightness":26},{"visibility":"on"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"hue":"#dddddd"},{"saturation":-100},{"lightness":-3},{"visibility":"on"}]}]';
    jsonDA2: any[] = [];
    arrayRutas2: any[] = [];
    latLon: any[] = [];
+
 
   ngOnInit() {
     this.Troncal = this._TroncalesService.getTroncales();
     this.latLon = this._TroncalesService.getLatLon();
     console.log(this.latLon);
+
   }
 
   // tslint:disable-next-line:max-line-length
-  constructor(private translate: TranslateService, public router: Router, private _TroncalesService: TroncalesService ,private _RutasService:RutasService) {
+  constructor(private translate: TranslateService, public router: Router, private _TroncalesService: TroncalesService , private _RutasService: RutasService) {
    // tslint:disable-next-line:max-line-length
    $.getJSON('http://datosabiertos.bogota.gov.co/api/3/action/datastore_search?resource_id=d0775af7-1706-4404-8bea-387194287d73&limit=1000', function(data) {
     $.each(data.result.records, function(i, item) {
@@ -53,20 +58,27 @@ export class MapaComponent implements OnInit {
   });
   this.jsonDA2 = jsonDA;
   console.log(this.jsonDA2);
-  this._RutasService.getJSONrutas().subscribe(data=>{
-    for(var i=0; i<=data.length-1;i++){
+  this._RutasService.getJSONrutas().subscribe(data => {
+    for (let i = 0; i <= data.length - 1; i++) {
         this.arrayRutas2.push((data[i]));
     }
-    
-})
-console.log(this.arrayRutas2)
+
+});
+console.log(this.arrayRutas2);
 
 
 }
-comprobar(vagon:string,Estacion:string){
-  if(vagon.indexOf(Estacion)){
+comprobar(vagon: string, Estacion: string) {
+  if (vagon.indexOf(Estacion)) {
     return true;
   }
   return false;
+}
+resetMap(lati: number , long: number, zoom2: number) {
+
+  console.log(lati + ' lat ' + long + ' long ' + zoom2 + ' zoom');
+  this.lat = lati;
+  this.lng = long;
+  this.zoom = zoom2;
 }
 }
