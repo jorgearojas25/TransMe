ca41a211da9fa12c98be4aabc22b2afa3b20bded
julian tamayo <juliantamayo16@outlook.es>
2019-05-02 13:41:52 -0500

    mapa terminado

--- TransMe/src/app/home/home/mapa/mapa.component.ts
+++ TransMe/src/app/home/home/mapa/mapa.component.ts
@@ -29,6 +29,8 @@ export class MapaComponent implements OnInit {
    Troncal: any[] = [];
   Estacion: any[]  = [];
   Troncal2: String[];
+  rutas: any[] =[];
+  vagones:any[] =[];
    // tslint:disable-next-line:max-line-length
    jsonDA2: any[] = [];
    arrayRutas2: any[] = [];
@@ -41,7 +43,8 @@ export class MapaComponent implements OnInit {
     this.latLon = this._TroncalesService.getLatLon();
     console.log(this.latLon);
     
-
+    this.getVagones();
+    this.getRutas();
   }
 
   // tslint:disable-next-line:max-line-length
@@ -71,14 +74,72 @@ export class MapaComponent implements OnInit {
 console.log(this.arrayRutas2);
 
 }
+verVagones(dato_nombreEstacion) {
+
+
+  var vagonesFiltrados = $.grep(this.vagones,function(value) {
+    return value.nombreEstacion === dato_nombreEstacion;
+  });
+  console.log(vagonesFiltrados);
+
+  $('#rutapop').empty();
+  let countVagon = 0;
+  $("#rutapop").append('<h2><span class="badge badge-pill badge-danger style="font-size: 40px;">'+'Te encuentras en la estacion '+dato_nombreEstacion+'</span></h2>');
+   $.each(vagonesFiltrados, function(i, value) {
+     countVagon += 1;
+
+  $('#rutapop').append('<br>');
+  const vagonActual = 'Vagon '+ countVagon ;
+    
+  $("#rutapop").append('<span class="badge badge-secondary">'+vagonActual+'</span>');
+
+  this.verRutas(value.idVagon);
+
+  }.bind(this));
+
+}
+verRutas(dato_nombreVagon) {
+
+  console.log(dato_nombreVagon);
+  const rutasFiltradas = $.grep(this.rutas, function(value) {
+    return value.idVagon === dato_nombreVagon;
+  });
+  console.log(rutasFiltradas);
+  // $("#rutaspop").empty();
+  let html = '';
+  $.each(rutasFiltradas, function(index, value) {
+  html += `<div style="margin-left:10px; display:inline-block"> ${value.nombreRuta}</div>`;
+  });
+  $('#rutapop').append(html);
+
+
+}
+public getVagones() {
+  this._RutasService.getJSONvagones().subscribe(data => {
+      for (let i = 0; i <= data.length - 1; i++) {
+          this.vagones.push((data[i])) ; }
+});
+  console.log("vagones: "+this.vagones);
+}
+
+public getRutas() {
+  this._RutasService.getJSONrutas().subscribe(data => {
+      for (let i = 0; i <= data.length - 1; i++) {
+          this.rutas.push((data[i]));
+      }
+
+  });
+  console.log("rutas: "+this.rutas);
+}
 
 buscar() {
 
   this.latLon.forEach(element => {
 
    if (element.Estacion === $('#estacionCambio').val()) {
-
-      this.resetMap(parseFloat(element.Lat), parseFloat(element.Lon), 20);
+     
+        
+      this.resetMap(parseFloat(element.Lat), parseFloat(element.Lon), 20,element.Estacion);
    }
  });
 
@@ -88,8 +149,7 @@ buscar2() {
   this.latLon.forEach(element => {
 
    if (element.Estacion.toLowerCase() === $('#cambiobusqueda').val().toLowerCase() ) {
-
-      this.resetMap(parseFloat(element.Lat), parseFloat(element.Lon), 20);
+      this.resetMap(parseFloat(element.Lat), parseFloat(element.Lon), 20,element.Estacion);
    }
  });
 
@@ -101,13 +161,21 @@ comprobar(vagon: string, Estacion: string) {
   }
   return false;
 }
-resetMap(lati: number , long: number, zoom2: number) {
+resetMap(lati: number , long: number, zoom2: number,palabra:String) {
 
   console.log(lati + ' lat ' + long + ' long ' + zoom2 + ' zoom');
   this.lat = lati;
   this.lng = long;
   this.zoom = zoom2;
-}
+  if(palabra!=undefined){
+    
+     
+    this.verVagones(palabra);
+    }else{
+      $('#rutapop').empty();
+    }
+  }
+
 
 /* buscarImagen(id_ruta){
     id_ruta=id_ruta.toString();
@@ -136,8 +204,6 @@ resetMap(lati: number , long: number, zoom2: number) {
       }
 } */
 
-cleanImage(){
-  $("#img").empty();
-}
+
 
 }
